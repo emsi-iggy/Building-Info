5c2b9472519030bb7ce9cdf243d331d98817f9c8
emsi-iggy <michaelcoldpl@gmail.com>
2018-11-08 16:43:32 +0100

    Uaktualnienie kodu w klasie Pomieszczenie w folderze app

--- src/main/java/pl/put/poznan/pomieszczenie/app/PomieszczenieApplication.java
+++ src/main/java/pl/put/poznan/pomieszczenie/app/PomieszczenieApplication.java
@@ -1,4 +1,12 @@
 package pl.put.poznan.pomieszczenie.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"pl.put.poznan.pomieszczenie.rest"})
+
 public class PomieszczenieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PomieszczenieApplication.class, args);
+    }
 }
