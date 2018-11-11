9206dd73815b4415a8d2314b24c6bb344065fb4e
emsi-iggy <michaelcoldpl@gmail.com>
2018-11-11 16:04:00 +0100

    Usunięcie listy pomieszczeń

--- src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
@@ -2,21 +2,15 @@ package pl.put.poznan.buildingInfo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.put.poznan.buildingInfo.logic.Pomieszczenie;
+import pl.put.poznan.buildingInfo.logic.Budynek;
 import java.util.*;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildingInfo.rest"})
 
 public class buildingInfoApplication {
-    public static List <Pomieszczenie> listaPomieszczen;
+    //public static List <Budynek> listaBudynkow;
     public static void main(String[] args) {
-        listaPomieszczen = new ArrayList <>();
-
-        //Dodajmy sobie jakies ze dwa pomieszczenia
-        Pomieszczenie a = new Pomieszczenie(listaPomieszczen.size(),"pokoj",9.0,27.0,100.0,100.0);
-        listaPomieszczen.add(a);
-        Pomieszczenie b = new Pomieszczenie(listaPomieszczen.size(),"kuchnia",16.0,64.0,130.0,140.0);
-        listaPomieszczen.add(b);
+        //listaBudynkow = new ArrayList<>();
 
         SpringApplication.run(buildingInfoApplication.class, args);
         System.out.println("<SUKCES> Rozpoczynam działanie aplikacji.");
