840ae8d203a543d357fc78d3c27fb18950ad9c31
tomdziwood <tomdziwood@gmail.com>
2018-11-27 21:25:59 +0100

    Pomieszczenie - dodanie pola 'dataRemontu'
    Dodanie pola typu java.util.Date, które przedstawia datę wykonania ostatniego remontu.

--- src/main/java/pl/put/poznan/buildingInfo/logic/Pomieszczenie.java
+++ src/main/java/pl/put/poznan/buildingInfo/logic/Pomieszczenie.java
@@ -2,6 +2,8 @@ package pl.put.poznan.buildingInfo.logic;
 
 import pl.put.poznan.buildingInfo.logic.Lokalizacja;
 
+import java.util.Date;
+
 /**
  * Class Pomieszczenie represents class Lokalizacja with additional double typu attributes such as
  * area, cube, heating and light
@@ -11,6 +13,7 @@ public class Pomieszczenie extends Lokalizacja{
     private double cube;
     private double heating;
     private double light;
+    private Date dataRemontu;
 
     public Pomieszczenie() {
         super();
@@ -18,6 +21,7 @@ public class Pomieszczenie extends Lokalizacja{
         this.cube = 0.0;
         this.heating = 0.0;
         this.light = 0.0;
+        this.dataRemontu = new Date();
     }
 
     /**
@@ -41,6 +45,27 @@ public class Pomieszczenie extends Lokalizacja{
 
     /**
      *
+     * @param id variable inherited from the class Lokalizacja
+     *           here is passed to the Costructor method of the class Pomieszczenie
+     * @param nazwa variable inherited from the class Lokalizacja
+     *              here is passed to the Costructor method of the class Pomieszczenie
+     * @param area variable represents the area of the room
+     * @param cube variable represents the volume of the room
+     * @param heating variable represents the energy consumption [kWh] for heating the room per year
+     * @param light variable represents the total lighting power [W] in the room
+     * @param dataRemontu is the date when the last renovation of the room was made
+     */
+    public Pomieszczenie(int id, String nazwa, Date dataRemontu, double area, double cube, double heating, double light) {
+        super(id, nazwa);
+        this.area = area;
+        this.cube = cube;
+        this.heating = heating;
+        this.light = light;
+        this.dataRemontu = dataRemontu;
+    }
+
+    /**
+     *
      * @return the String containing names of variables and their values
      */
     @Override
@@ -52,6 +77,7 @@ public class Pomieszczenie extends Lokalizacja{
                 ", cube=" + cube +
                 ", heating=" + heating +
                 ", light=" + light +
+                ", dataRemontu=" + dataRemontu +
                 '}';
     }
 
@@ -122,6 +148,23 @@ public class Pomieszczenie extends Lokalizacja{
      */
     public void setArea(double area) {
         this.area = area;
+    }
+
+    /**
+     *
+     * @return the variable dataRemontu
+     */
+    public Date getDataRemontu() {
+        return dataRemontu;
+    }
+
+    /**
+     *
+     * @param dataRemontu the same as described above, but here is passed to the setDataRemontu method,
+     *                    so we can set the value of the dataRemontu
+     */
+    public void setDataRemontu(Date dataRemontu) {
+        this.dataRemontu = dataRemontu;
     }
 
     /**
