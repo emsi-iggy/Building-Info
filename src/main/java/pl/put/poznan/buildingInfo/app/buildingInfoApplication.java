792eb725dd7b5d23c03cc8486ec7b3224e559b41
emsi-iggy <michaelcoldpl@gmail.com>
2018-11-29 14:47:04 +0100

    Aktualizacja klasy buildingInfoApplication
    dodanie okien do poszczególnych pomieszczeń

--- src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
@@ -2,12 +2,13 @@ package pl.put.poznan.buildingInfo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.put.poznan.buildingInfo.logic.Budynek;
-import pl.put.poznan.buildingInfo.logic.JSONCreator;
-import pl.put.poznan.buildingInfo.logic.Pomieszczenie;
-import pl.put.poznan.buildingInfo.logic.Poziom;
+import pl.put.poznan.buildingInfo.logic.*;
 
+import java.lang.reflect.Array;
 import java.util.*;
+
+import static pl.put.poznan.buildingInfo.logic.Okno.RodzajOknaPCV.*;
+import static pl.put.poznan.buildingInfo.logic.Okno.SposobOtwarciaOkna.*;
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.buildingInfo.rest"})
 
@@ -22,6 +23,22 @@ public class buildingInfoApplication {
         Pomieszczenie kuchnia = new Pomieszczenie(0,"Kuchnia",14,43,420, 42);
         Pomieszczenie lazienka = new Pomieszczenie(1,"Łazienka",16,40,640,80);
         Pomieszczenie salon = new Pomieszczenie(2,"Salon",25,70,1325,75);
+        Okno oknoKuchnia1 = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
+        Okno oknoKuchnia2 = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
+        Okno oknoLazienka1 = new Okno(1.0,0.5,350, TRADYCYJNE, UCHYLNOROZWIERNE);
+        ArrayList <Okno> listaOkienWKuchni = new ArrayList<>();
+        ArrayList <Okno> listaOkienWLazience = new ArrayList<>();
+        ArrayList <Okno> listaOkienWSalonie = new ArrayList<>();
+
+        listaOkienWKuchni.add(oknoKuchnia1);
+        listaOkienWKuchni.add(oknoKuchnia2);
+        listaOkienWLazience.add(oknoLazienka1);
+        listaOkienWSalonie.add(oknoKuchnia1);
+        listaOkienWSalonie.add(oknoKuchnia2);
+
+        kuchnia.setListaOkien(listaOkienWKuchni);
+        lazienka.setListaOkien(listaOkienWLazience);
+        salon.setListaOkien(listaOkienWSalonie);
 
         listaPomieszczenParter.add(kuchnia);
         listaPomieszczenParter.add(lazienka);
@@ -30,6 +47,16 @@ public class buildingInfoApplication {
         ArrayList <Pomieszczenie> listaPomieszczenPierwszePietro = new ArrayList<>();
         Pomieszczenie sypialnia = new Pomieszczenie(0,"Sypialnia",20,50,800,80);
         Pomieszczenie toaleta = new Pomieszczenie(1,"Toaleta",9,18,270,27);
+
+        Okno oknoSypialnia1 = new Okno(2.3,1.4,780,BALKONOWE, PRZESUWNE);
+        Okno oknoToaleta1 = new Okno(1.0,0.5,339.99,TRADYCYJNE,UCHYLNE);
+        ArrayList <Okno>  listaOkienWSypialni = new ArrayList<>();
+        ArrayList <Okno> listaOkienWToalecie = new ArrayList<>();
+        listaOkienWSypialni.add(oknoSypialnia1);
+        listaOkienWToalecie.add(oknoToaleta1);
+
+        sypialnia.setListaOkien(listaOkienWSypialni);
+        toaleta.setListaOkien(listaOkienWToalecie);
 
         listaPomieszczenPierwszePietro.add(sypialnia);
         listaPomieszczenPierwszePietro.add(toaleta);
