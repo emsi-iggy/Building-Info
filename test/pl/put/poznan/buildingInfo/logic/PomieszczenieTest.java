5e5563daa1f287ca89793f22a80a19ae50dfb049
emsi-iggy <michaelcoldpl@gmail.com>
2018-12-07 18:30:48 +0100

    Dodanie klasy testującej dla klasy Pomieszczenie
    Ta klasa będzie odpowiadać za testy jednostkowe dla klasy pomieszczenie.
    Jest to dopiero szkic tej klasy.

---
+++ test/pl/put/poznan/buildingInfo/logic/PomieszczenieTest.java
@@ -0,0 +1,41 @@
+package pl.put.poznan.buildingInfo.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static pl.put.poznan.buildingInfo.logic.Okno.RodzajOknaPCV.TRADYCYJNE;
+import static pl.put.poznan.buildingInfo.logic.Okno.SposobOtwarciaOkna.UCHYLNOROZWIERNE;
+
+public class PomieszczenieTest {
+    Pomieszczenie pomieszczenie;
+    ArrayList <Okno> listaOkien;
+
+    @Before
+    public void setUp() {
+        pomieszczenie = new Pomieszczenie();
+        listaOkien = new ArrayList<>();
+    }
+
+    @After
+    public void tearDown() {
+        pomieszczenie = null;
+        listaOkien = null;
+    }
+
+    //Trzeba jeszcze wypelnic
+    @Test
+    public void testGetPowierzchniaOkienLiczbyCalkowite() {
+        listaOkien.add(new Okno(3, 5, 940, TRADYCYJNE, UCHYLNOROZWIERNE));
+        assertEquals(3*5,pomieszczenie.getPowierzchniaOkien(),0.001);
+    }
+
+    @Test
+    public void testGetPowierzchniaOkienLiczbyRzeczywiste() {
+        listaOkien.add(new Okno(1.2,0.7,430,TRADYCYJNE,UCHYLNOROZWIERNE));
+        assertEquals(3*5,pomieszczenie.getPowierzchniaOkien(),0.001);
+    }
+}
