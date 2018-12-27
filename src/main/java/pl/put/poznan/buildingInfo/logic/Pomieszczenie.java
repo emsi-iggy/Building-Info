9c51a568f6d2b24e6bf5b9c95a055661700bdba5
tomdziwood <tomdziwood@gmail.com>
2018-12-27 16:17:13 +0100

    Akcje naprawcze - dokumentacja
    Uporządkowano i zaktualizowano dokumentację klasy 'Pomieszczenie'.

--- src/main/java/pl/put/poznan/buildingInfo/logic/Pomieszczenie.java
+++ src/main/java/pl/put/poznan/buildingInfo/logic/Pomieszczenie.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * Class Pomieszczenie represents class Lokalizacja with additional double type attributes such as
- * area, cube, heating and light
+ * Class {@link Pomieszczenie} represents class {@link Lokalizacja} with additional double type attributes such as
+ * {@code area}, {@code cube}, {@code heating} and {@code light}
  */
 public class Pomieszczenie extends Lokalizacja{
     private double area;
@@ -18,6 +18,10 @@ public class Pomieszczenie extends Lokalizacja{
     private Date dataRemontu;
     private ArrayList <Okno> listaOkien;
 
+    /**
+     * This is an empty constructor of the {@link Pomieszczenie} which allows to set the basic properties of the object
+     * to the default null values.
+     */
     public Pomieszczenie() {
         super();
         this.area = 0.0;
@@ -29,6 +33,8 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * This is a constructor of the {@link Pomieszczenie} which allows to set the basic properties of the object
+     * with given parameters values.
      *
      * @param id variable inherited from the class Lokalizacja
      *           here is passed to the Costructor method of the class Pomieszczenie
@@ -50,16 +56,20 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>This is a constructor of the {@link Pomieszczenie} which allows to set the basic properties of the object
+     * with given parameters values.</p>
+     * <p>Compared with constructor with six arguments, this one has additional argument,
+     * which sets the date of the last renovation of the room.</p>
      *
      * @param id variable inherited from the class Lokalizacja
      *           here is passed to the Costructor method of the class Pomieszczenie
      * @param nazwa variable inherited from the class Lokalizacja
      *              here is passed to the Costructor method of the class Pomieszczenie
+     * @param dataRemontu is the date when the last renovation of the room was made
      * @param area variable represents the area of the room
      * @param cube variable represents the volume of the room
      * @param heating variable represents the energy consumption [kWh] for heating the room per year
      * @param light variable represents the total lighting power [W] in the room
-     * @param dataRemontu is the date when the last renovation of the room was made
      */
     public Pomieszczenie(int id, String nazwa, Date dataRemontu, double area, double cube, double heating, double light) {
         super(id, nazwa);
@@ -72,17 +82,21 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>This is a constructor of the {@link Pomieszczenie} which allows to set the basic properties of the object
+     * with given parameters values.</p>
+     * <p>Compared with constructor with seven arguments, this one has additional argument,
+     * which sets the list containing all th windows in the room.</p>
      *
      * @param id variable inherited from the class Lokalizacja
      *           here is passed to the Costructor method of the class Pomieszczenie
      * @param nazwa variable inherited from the class Lokalizacja
      *              here is passed to the Costructor method of the class Pomieszczenie
+     * @param dataRemontu is the date when the last renovation of the room was made
+     * @param listaOkien is the list containing all the windows in the room
      * @param area variable represents the area of the room
      * @param cube variable represents the volume of the room
      * @param heating variable represents the energy consumption [kWh] for heating the room per year
      * @param light variable represents the total lighting power [W] in the room
-     * @param dataRemontu is the date when the last renovation of the room was made
-     * @param listaOkien is the list containing all the windows in the room
      */
     public Pomieszczenie(int id, String nazwa, Date dataRemontu, ArrayList<Okno> listaOkien, double area, double cube, double heating, double light) {
         super(id, nazwa);
@@ -95,6 +109,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * This is an overridden method toString. It allows to print on the screen basic info about an object of the {@link Pomieszczenie}
      *
      * @return the String containing names of variables and their values
      */
@@ -113,58 +128,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
-     *
-     * @return the variable cube
-     */
-    public double getCube() {
-        return cube;
-    }
-
-    /**
-     *
-     * @param cube the same as described above, but here is passed to the setCube method,
-     *             so we can set the value of the volume
-     */
-    public void setCube(double cube) {
-        this.cube = cube;
-    }
-
-    /**
-     *
-     * @return the variable heating
-     */
-
-    public double getHeating() {
-        return heating;
-    }
-
-    /**
-     *
-     * @param heating the same as described above, but here is passed to the setHeating method,
-     *                so we can set the value of the heating consumption
-     */
-    public void setHeating(double heating) {
-        this.heating = heating;
-    }
-
-    /**
-     *
-     * @return the variable light
-     */
-    public double getLight() {
-        return light;
-    }
-
-    /**
-     *
-     * @param light the same as described above, but here is passed to the setLight method,
-     *              so we can set the value of the power lighting
-     */
-    public void setLight(double light) {
-        this.light = light;
-    }
-
-    /**
+     * Gets the value of the private field {@code area}. It is a variable represents the area of the room.
      *
      * @return the variable area
      */
@@ -173,6 +137,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * Sets the value of the private field {@code area}. It is a variable represents the area of the room.
      *
      * @param area the same as described above, but here is passed to the setArea method,
      *             so we can set the value of the area
@@ -182,6 +147,64 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * Gets the value of the private field {@code cube}. It is a variable represents the volume of the room.
+     *
+     * @return the variable cube
+     */
+    public double getCube() {
+        return cube;
+    }
+
+    /**
+     * Sets the value of the private field {@code cube}. It is a variable represents the volume of the room.
+     *
+     * @param cube the same as described above, but here is passed to the setCube method,
+     *             so we can set the value of the volume
+     */
+    public void setCube(double cube) {
+        this.cube = cube;
+    }
+
+    /**
+     * Gets the value of the private field {@code heating}. It is a variable represents the energy consumption [kWh] for heating the room per year.
+     *
+     * @return the variable heating
+     */
+    public double getHeating() {
+        return heating;
+    }
+
+    /**
+     * Sets the value of the private field {@code heating}. It is a variable represents the energy consumption [kWh] for heating the room per year.
+     *
+     * @param heating the same as described above, but here is passed to the setHeating method,
+     *                so we can set the value of the heating consumption
+     */
+    public void setHeating(double heating) {
+        this.heating = heating;
+    }
+
+    /**
+     * Gets the value of the private field {@code light}. It is a variable represents the total lighting power [W] in the room.
+     *
+     * @return the variable light
+     */
+    public double getLight() {
+        return light;
+    }
+
+    /**
+     * Sets the value of the private field {@code light}. It is a variable represents the total lighting power [W] in the room.
+     *
+     * @param light the same as described above, but here is passed to the setLight method,
+     *              so we can set the value of the power lighting
+     */
+    public void setLight(double light) {
+        this.light = light;
+    }
+
+    /**
+     * Gets the value of the private field {@code dataRemontu}. It is the date when the last renovation of the room was made.
      *
      * @return the variable dataRemontu
      */
@@ -190,6 +213,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * Sets the value of the private field {@code dataRemontu}. It is the date when the last renovation of the room was made.
      *
      * @param dataRemontu the same as described above, but here is passed to the setDataRemontu method,
      *                    so we can set the value of the dataRemontu
@@ -199,6 +223,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * Gets the value of the private field {@code listaOkien}. It is the list containing all the windows in the room.
      *
      * @return the list listaOkien
      */
@@ -207,6 +232,7 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * Sets the value of the private field {@code listaOkien}. It is the list containing all the windows in the room.
      *
      * @param listaOkien the same as described above, but here is passed to the setListaOkien method,
      *                   so we can set values of the list listaOkien
@@ -216,6 +242,8 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>Gets the value of the total area of all windows placed in the room.</p>
+     * <p>The method iterates over the {@code listaOkien} and accumulates the area of every window.</p>
      *
      * @return the sum of Building's windows areas from the room (describes the total windows area of the room)
      */
@@ -230,6 +258,8 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>Gets the value of the total number of all windows placed in the room.</p>
+     * <p>The method takes the size of the {@code listaOkien} list.</p>
      *
      * @return the nubmer of Building's windows from the room (describes the total windows number of the room)
      */
@@ -238,6 +268,9 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>Gets the value of the coefficient of lighting power to the area of the room.</p>
+     * <p>The method calculate ratio of the value {@link Pomieszczenie#getLight()} to the value {@link Pomieszczenie#getArea()}.
+     * In case of {@code area} equals {@code 0}, the method returns {@code 0}.</p>
      *
      * @return the number representing proportion of the lighting power to the area of the room
      */
@@ -250,6 +283,9 @@ public class Pomieszczenie extends Lokalizacja{
     }
 
     /**
+     * <p>Gets the value of the coefficient of the energy consumption for heating to the volume of the room.</p>
+     * <p>The method calculate ratio of the value {@link Pomieszczenie#getLight()} to the value {@link Pomieszczenie#getCube()}.
+     * In case of {@code cube} equals {@code 0}, the method returns {@code 0}.</p>
      *
      * @return the number representing proportion of the energy consumption for heating to the volume of the room
      */
