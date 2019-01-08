4ebda93ed51230f17a09241cfe0c90b7756bc170
tomdziwood <tomdziwood@gmail.com>
2019-01-08 14:40:43 +0100

    Poprawa czytelności kodu
    Dodanie budynku 0 zostało wykonane w odpowiedniej do tej czynności funkcji.

--- src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
+++ src/main/java/pl/put/poznan/buildingInfo/app/buildingInfoApplication.java
@@ -20,6 +20,55 @@ public class buildingInfoApplication {
      */
     public static List <Budynek> listaBudynkow;
 
+    private static Budynek stworzBudynek1() {
+        Okno okno[] = new Okno[5];
+        ArrayList <Okno> listaOkien;
+        ArrayList <Pomieszczenie> listaPomieszczen;
+        ArrayList <Poziom> listaPoziomow;
+        Budynek budynek;
+
+
+        okno[0] = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
+        okno[1] = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
+        okno[2] = new Okno(1.0,0.5,350, TRADYCYJNE, UCHYLNOROZWIERNE);
+        okno[3] = new Okno(2.3,1.4,780,BALKONOWE, PRZESUWNE);
+        okno[4] = new Okno(1.0,0.5,339.99,TRADYCYJNE,UCHYLNE);
+
+        listaOkien = new ArrayList<>();
+        listaOkien.add(okno[0]);
+        listaOkien.add(okno[1]);
+        listaPomieszczen = new ArrayList<>();
+        listaPomieszczen.add(new Pomieszczenie(0,"Kuchnia", new Date(105, 10, 20), listaOkien, 14,43,420, 42));
+
+        listaOkien = new ArrayList<>();
+        listaOkien.add(okno[2]);
+        listaPomieszczen.add(new Pomieszczenie(1,"Łazienka", new Date(110, 7, 24), listaOkien,16,40,640,80));
+
+        listaOkien = new ArrayList<>();
+        listaOkien.add(okno[0]);
+        listaOkien.add(okno[1]);
+        listaPomieszczen.add(new Pomieszczenie(2,"Salon", new Date(102, 5, 29), listaOkien,25,70,1325,75));
+
+        listaPoziomow = new ArrayList<>();
+        listaPoziomow.add(new Poziom(0, "Parter", listaPomieszczen));
+
+
+        listaOkien = new ArrayList<>();
+        listaOkien.add(okno[3]);
+        listaPomieszczen = new ArrayList<>();
+        listaPomieszczen.add(new Pomieszczenie(0,"Sypialnia", new Date(109, 9, 13), listaOkien,20,50,800,80));
+
+        listaOkien = new ArrayList<>();
+        listaOkien.add(okno[4]);
+        listaPomieszczen.add(new Pomieszczenie(1,"Toaleta", new Date(102, 5, 29), listaOkien,9,18,270,27));
+
+        listaPoziomow.add(new Poziom(1, "Pierwsze piętro", listaPomieszczen));
+
+        budynek = new Budynek(0, "Moj dom", listaPoziomow);
+
+        return budynek;
+    }
+
     private static Budynek stworzBudynek2() {
         Okno okno[] = new Okno[14];
         ArrayList <Okno> listaOkien;
@@ -49,34 +98,34 @@ public class buildingInfoApplication {
         listaOkien.add(okno[6]);
         listaOkien.add(okno[13]);
         listaPomieszczen = new ArrayList<>();
-        listaPomieszczen.add(new Pomieszczenie(0, "Kuchnia", new Date(113, 3, 20), listaOkien, 22.14, 55.35, 29968, 68));
+        listaPomieszczen.add(new Pomieszczenie(0, "Kuchnia", new Date(113, 3, 20), listaOkien, 22.14, 55.35, 2996, 68));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[1]);
         listaOkien.add(okno[1]);
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(1, "Salon", new Date(104, 6, 6), listaOkien, 18.9, 47.25, 16346, 180));
+        listaPomieszczen.add(new Pomieszczenie(1, "Salon", new Date(104, 6, 6), listaOkien, 18.9, 47.25, 1634, 180));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[1]);
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(2, "Sypialnia", new Date(104, 6, 6), listaOkien, 13.5, 33.75, 10897, 90));
+        listaPomieszczen.add(new Pomieszczenie(2, "Sypialnia", new Date(104, 6, 6), listaOkien, 13.5, 33.75, 1089, 90));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[5]);
-        listaPomieszczen.add(new Pomieszczenie(3, "Łazienka", new Date(113, 3, 20), listaOkien, 3.38, 8.45, 21794, 20));
+        listaPomieszczen.add(new Pomieszczenie(3, "Łazienka", new Date(113, 3, 20), listaOkien, 3.38, 8.45, 2179, 20));
 
         listaOkien = new ArrayList<>();
-        listaPomieszczen.add(new Pomieszczenie(4, "Korytarz", new Date(113, 3, 20), listaOkien, 4.81, 12.025, 8173, 60));
+        listaPomieszczen.add(new Pomieszczenie(4, "Korytarz", new Date(113, 3, 20), listaOkien, 4.81, 12.025, 817, 60));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(5, "Garderoba", new Date(106, 8, 17), listaOkien, 5.85, 14.625, 2724, 60));
+        listaPomieszczen.add(new Pomieszczenie(5, "Garderoba", new Date(106, 8, 17), listaOkien, 5.85, 14.625, 272, 60));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[2]);
         listaOkien.add(okno[9]);
-        listaPomieszczen.add(new Pomieszczenie(6, "Altana", new Date(113, 3, 20), listaOkien, 8.5, 21.25, 19070, 75));
+        listaPomieszczen.add(new Pomieszczenie(6, "Altana", new Date(113, 3, 20), listaOkien, 8.5, 21.25, 1907, 75));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[11]);
@@ -91,7 +140,7 @@ public class buildingInfoApplication {
         listaOkien.add(okno[9]);
         listaOkien.add(okno[10]);
         listaPomieszczen = new ArrayList<>();
-        listaPomieszczen.add(new Pomieszczenie(0, "Pokój", new Date(110, 9, 23), listaOkien, 8.5, 21.25, 13621, 120));
+        listaPomieszczen.add(new Pomieszczenie(0, "Pokój", new Date(110, 9, 23), listaOkien, 8.5, 21.25, 1362, 120));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[1]);
@@ -100,30 +149,30 @@ public class buildingInfoApplication {
         listaOkien.add(okno[1]);
         listaOkien.add(okno[1]);
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(1, "Salon", new Date(110, 9, 23), listaOkien, 18.9, 47.25, 14166, 26));
+        listaPomieszczen.add(new Pomieszczenie(1, "Salon", new Date(110, 9, 23), listaOkien, 18.9, 47.25, 1416, 26));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[1]);
         listaOkien.add(okno[1]);
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(2, "Sypialnia", new Date(110, 9, 23), listaOkien, 19.35, 48.375, 13076, 135));
+        listaPomieszczen.add(new Pomieszczenie(2, "Sypialnia", new Date(110, 9, 23), listaOkien, 19.35, 48.375, 1307, 135));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[0]);
         listaOkien.add(okno[7]);
-        listaPomieszczen.add(new Pomieszczenie(3, "Pokój", new Date(110, 9, 23), listaOkien, 10, 25, 7628, 7));
+        listaPomieszczen.add(new Pomieszczenie(3, "Pokój", new Date(110, 9, 23), listaOkien, 10, 25, 762, 7));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[0]);
         listaOkien.add(okno[7]);
-        listaPomieszczen.add(new Pomieszczenie(4, "Pokój", new Date(106, 8, 17), listaOkien, 7.28, 18.2, 5448, 10));
+        listaPomieszczen.add(new Pomieszczenie(4, "Pokój", new Date(106, 8, 17), listaOkien, 7.28, 18.2, 544, 10));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[8]);
-        listaPomieszczen.add(new Pomieszczenie(5, "Łazienka", new Date(105, 8, 10), listaOkien, 3.24, 8.1, 2724, 70));
+        listaPomieszczen.add(new Pomieszczenie(5, "Łazienka", new Date(105, 8, 10), listaOkien, 3.24, 8.1, 272, 70));
 
         listaOkien = new ArrayList<>();
-        listaPomieszczen.add(new Pomieszczenie(6, "Korytarz", new Date(105, 8, 10), listaOkien, 8.58, 21.45, 5448, 180));
+        listaPomieszczen.add(new Pomieszczenie(6, "Korytarz", new Date(105, 8, 10), listaOkien, 8.58, 21.45, 544, 180));
 
         listaOkien = new ArrayList<>();
         listaOkien.add(okno[11]);
@@ -147,57 +196,7 @@ public class buildingInfoApplication {
         listaBudynkow = new ArrayList<>();
         JSONCreator kreator = new JSONCreator();
 
-        ArrayList <Pomieszczenie> listaPomieszczenParter = new ArrayList<>();
-        Pomieszczenie kuchnia = new Pomieszczenie(0,"Kuchnia",14,43,420, 42);
-        Pomieszczenie lazienka = new Pomieszczenie(1,"Łazienka",16,40,640,80);
-        Pomieszczenie salon = new Pomieszczenie(2,"Salon",25,70,1325,75);
-        Okno oknoKuchnia1 = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
-        Okno oknoKuchnia2 = new Okno(1.17, 1.14, 540, TRADYCYJNE, UCHYLNOROZWIERNE);
-        Okno oknoLazienka1 = new Okno(1.0,0.5,350, TRADYCYJNE, UCHYLNOROZWIERNE);
-        ArrayList <Okno> listaOkienWKuchni = new ArrayList<>();
-        ArrayList <Okno> listaOkienWLazience = new ArrayList<>();
-        ArrayList <Okno> listaOkienWSalonie = new ArrayList<>();
-
-        listaOkienWKuchni.add(oknoKuchnia1);
-        listaOkienWKuchni.add(oknoKuchnia2);
-        listaOkienWLazience.add(oknoLazienka1);
-        listaOkienWSalonie.add(oknoKuchnia1);
-        listaOkienWSalonie.add(oknoKuchnia2);
-
-        kuchnia.setListaOkien(listaOkienWKuchni);
-        lazienka.setListaOkien(listaOkienWLazience);
-        salon.setListaOkien(listaOkienWSalonie);
-
-        listaPomieszczenParter.add(kuchnia);
-        listaPomieszczenParter.add(lazienka);
-        listaPomieszczenParter.add(salon);
-
-        ArrayList <Pomieszczenie> listaPomieszczenPierwszePietro = new ArrayList<>();
-        Pomieszczenie sypialnia = new Pomieszczenie(0,"Sypialnia",20,50,800,80);
-        Pomieszczenie toaleta = new Pomieszczenie(1,"Toaleta",9,18,270,27);
-
-        Okno oknoSypialnia1 = new Okno(2.3,1.4,780,BALKONOWE, PRZESUWNE);
-        Okno oknoToaleta1 = new Okno(1.0,0.5,339.99,TRADYCYJNE,UCHYLNE);
-        ArrayList <Okno>  listaOkienWSypialni = new ArrayList<>();
-        ArrayList <Okno> listaOkienWToalecie = new ArrayList<>();
-        listaOkienWSypialni.add(oknoSypialnia1);
-        listaOkienWToalecie.add(oknoToaleta1);
-
-        sypialnia.setListaOkien(listaOkienWSypialni);
-        toaleta.setListaOkien(listaOkienWToalecie);
-
-        listaPomieszczenPierwszePietro.add(sypialnia);
-        listaPomieszczenPierwszePietro.add(toaleta);
-
-        ArrayList <Poziom> listaPoziomowDomu = new ArrayList<>();
-        Poziom parter = new Poziom(0,"Parter",listaPomieszczenParter);
-        Poziom pierwszePietro = new Poziom(1,"Pierwsze piętro", listaPomieszczenPierwszePietro);
-        listaPoziomowDomu.add(parter);
-        listaPoziomowDomu.add(pierwszePietro);
-
-        Budynek dom = new Budynek(0,"Mój dom",listaPoziomowDomu);
-
-        listaBudynkow.add(dom);
+        listaBudynkow.add(stworzBudynek1());
         listaBudynkow.add(stworzBudynek2());
 
         SpringApplication.run(buildingInfoApplication.class, args);
